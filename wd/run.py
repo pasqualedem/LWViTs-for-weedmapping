57e22fadee399551bc1e493954d5b25f0577cecf
Pasquale De Marinis <pasquale199876@hotmail.it>
2022-05-06 12:08:31 +0200

    Experiment enhancer

--- wd/run.py
+++ wd/run.py
@@ -24,7 +24,6 @@ from wd.utils.utils import values_to_number
 from wd.learning.seg_trainer import SegmentationTrainer
 from wd.learning.wandb_logger import WandBSGLogger
 
-
 logger = get_logger(__name__)
 
 
@@ -139,37 +138,67 @@ def inference(seg_trainer, run_params, dataset):
 
 def experiment(settings: Mapping, param_path: str = "local variable"):
     exp_settings = settings['experiment']
-    grids = settings['parameters']
+    base_grid = settings['parameters']
     other_grids = settings['other_grids']
+    starting_grid = exp_settings['start_from_grid']
+    starting_run = exp_settings['start_from_run']
+
+    exp_log = open('exp_log.txt', 'a')
+    exp_log.write('---')
+    exp_log.flush()
 
     if exp_settings['excluded_files']:
         os.environ['WANDB_IGNORE_GLOBS'] = exp_settings['excluded_files']
 
     logger.info(f'Loaded parameters from {param_path}')
-    runs = make_grid(grids)
-    logger.info(f'Found {len(runs)} runs from base grid')
 
+    complete_grids = [base_grid]
     if other_grids:
-        complete_other_grids =\
-            [nested_dict_update(copy.deepcopy(settings['parameters']), other_run) for other_run in other_grids]
-        other_runs = [make_grid(other_grid) for other_grid in complete_other_grids]
-        logger.info(f'There are {len(other_grids)} other grids')
-        for i, other_run in enumerate(other_runs):
-            logger.info(f'Found {len(other_run)} runs from grid {i + 1}')
-            runs = runs + other_run
-    logger.info(f'Total runs: {len(runs)}')
+        complete_grids += \
+            [nested_dict_update(copy.deepcopy(base_grid), other_run) for other_run in other_grids]
+    logger.info(f'There are {len(complete_grids)} grids')
+
+    grids = []
+    for i, grid in enumerate(complete_grids):
+        grid_runs = make_grid(grid)
+        info = f'Found {len(grid_runs)} runs from grid {i}'
+        if i < starting_grid:
+            info += f', skipping grid {i} with {len(grid_runs)} runs'
+        grids = grids + [grid_runs]
+        logger.info(info)
+
+    total_runs = sum(len(grid) for grid in grids)
+    total_runs_excl_grid = total_runs - sum([len(grid) for grid in grids[starting_grid:]])
+    total_runs_excl = total_runs_excl_grid + starting_run
+    total_runs_to_run = total_runs - total_runs_excl
+    logger.info(f'Total runs found:              {total_runs}')
+    logger.info(f'Total runs excluded by grids:  {total_runs_excl_grid}')
+    logger.info(f'Total runs excluded:           {total_runs_excl}')
+    logger.info(f'Total runs to run:             {total_runs_to_run}')
 
     continue_with_errors = exp_settings.pop('continue_with_errors')
 
-    for i, params in enumerate(runs):
-        try:
-            logger.info(f'Running experiment {i + 1} out of {len(runs)}')
-            run({'experiment': exp_settings, **params})
-            gc.collect()
-        except Exception as e:
-            logger.error(f'Experiment {i + 1} failed with error {e}')
-            if not continue_with_errors:
-                raise e
+    for i in range(starting_grid, len(grids)):
+        grid = grids[i]
+        if i != starting_grid:
+            starting_run = 0
+        for j in range(starting_run, len(grid)):
+            params = grid[j]
+            try:
+                logger.info(f'Running grid {i} out of {len(grids) - 1}')
+                logger.info(f'Running run {j} out of {len(grid) - 1} '
+                            f'({sum([len(grids[k]) for k in range(i)]) + j} / {total_runs - 1})')
+                run({'experiment': exp_settings, **params})
+                exp_log.write(f'{i} {j}, finished \n')
+                exp_log.flush()
+                gc.collect()
+            except Exception as e:
+                logger.error(f'Experiment {i} failed with error {e}')
+                exp_log.write(f'{i} {j}, crashed \n')
+                exp_log.flush()
+                if not continue_with_errors:
+                    raise e
+    exp_log.close()
 
 
 def resume(settings):
@@ -229,4 +258,3 @@ if __name__ == '__main__':
             settings = YAML().load(param_stream)
 
         experiment(settings)
-
