d8bb8f0efa3c3123fd8e3807b54c476c0c7f5459
Pasquale De Marinis <pasquale199876@hotmail.it>
2022-05-17 21:29:00 +0200

    Froze layers

--- wd/models/base.py
+++ wd/models/base.py
@@ -1,15 +1,19 @@
 import torch
 import math
+
+from super_gradients.training.utils import HpmStruct
+from super_gradients.training import utils as sg_utils
 from torch import nn
 from wd.models.backbones import *
 from wd.models.layers import trunc_normal_
 
 
 class BaseModel(nn.Module):
-    def __init__(self, backbone: str = 'MiT-B0', input_channels: int = 3, pretrained: bool = False) -> None:
+    def __init__(self, backbone: str = 'MiT-B0', input_channels: int = 3, backbone_pretrained: bool = False) -> None:
         super().__init__()
         backbone, variant = backbone.split('-')
-        self.backbone = eval(backbone)(variant, input_channels, pretrained)
+        self.backbone_pretrained = backbone_pretrained
+        self.backbone = eval(backbone)(variant, input_channels, backbone_pretrained)
 
     def _init_weights(self, m: nn.Module) -> None:
         if isinstance(m, nn.Linear):
@@ -29,3 +33,13 @@ class BaseModel(nn.Module):
     def init_pretrained(self, pretrained: str = None) -> None:
         if pretrained:
             self.backbone.load_state_dict(torch.load(pretrained, map_location='cpu'), strict=False)
+
+    def initialize_param_groups(self, lr: float, training_params: HpmStruct) -> list:
+        """
+
+        :return: list of dictionaries containing the key 'named_params' with a list of named params
+        """
+        freeze_pretrained = sg_utils.get_param(training_params, 'freeze_pretrained', False)
+        if self.backbone_pretrained and freeze_pretrained:
+            return [{'named_params': list(filter(lambda x: not x[0].startswith('backbone'), list(self.named_parameters())))}]
+        return [{'named_params': self.named_parameters()}]
