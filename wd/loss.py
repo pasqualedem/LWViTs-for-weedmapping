89368d585e90473cfe0f0c0b1cf557c2fad297b7
Pasquale De Marinis <pasquale199876@hotmail.it>
2022-05-18 20:46:53 +0200

    Focal Loss

--- wd/loss.py
+++ wd/loss.py
@@ -1,4 +1,5 @@
-from torch.nn import CrossEntropyLoss
+from torch.nn import CrossEntropyLoss, Module
+import torch.nn.functional as F
 import torch
 
 
@@ -12,6 +13,39 @@ class CELoss(CrossEntropyLoss):
         return super().__call__(x, target.float(), **kwargs)
 
 
+class FocalLoss(Module):
+    def __init__(self, alpha: float = 1, gamma: float = 0, weight=None, reduction: str = 'none', **kwargs):
+        super().__init__()
+        if weight:
+            self.weight = torch.tensor(weight)
+        self.gamma = gamma
+
+        if reduction == 'none':
+            self.reduction = lambda x: x
+        elif reduction == 'mean':
+            self.reduction = torch.mean
+        elif reduction == 'sum':
+            self.reduction = torch.sum
+        else:
+            raise NotImplementedError(f"Invalid reduction mode: {self.reduction}")
+
+    def __call__(self, x, target, **kwargs):
+        soft = F.softmax(x, 1)
+        if self.weight is not None:
+            wtarget = self.weight * target
+        else:
+            wtarget = target
+
+        pt = soft[wtarget != 0]
+        ce = -(torch.log(pt) * wtarget[wtarget != 0])
+
+        focus = torch.pow(-soft + 1.0, self.gamma)
+        focal = (focus * ce)
+
+        return self.reduction(focal)
+
+
 LOSSES = {
     'cross_entropy': CELoss,
+    'focal': FocalLoss
 }
