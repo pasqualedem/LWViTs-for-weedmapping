02e777884a5bac9c283f37261a4751da5ac8c8b0
Pasquale De Marinis <pasquale199876@hotmail.it>
2022-06-30 17:44:25 +0200

    MLflow dismission

--- wd/utils/utilities.py
+++ wd/utils/utilities.py
@@ -7,72 +7,71 @@ from typing import Any, Optional, Mapping
 import mlflow
 import torch
 import collections.abc
-from mlflow.tracking import MlflowClient
 from ruamel.yaml import YAML
 
-
-def setup_mlflow(exp_name: str, description: str) -> str:
-    """
-    Setup mlflow tracking server and experiment
-    """
-    tracking_uri = registry_uri = "http://localhost:5000"
-    mlflow.set_tracking_uri(tracking_uri)
-    mlflow.set_registry_uri(registry_uri)
-    print('URI set!')
-    client = MlflowClient(tracking_uri, registry_uri)
-    exp_info = client.get_experiment_by_name(exp_name)
-    exp_id = exp_info.experiment_id if exp_info else \
-        client.create_experiment(exp_name, tags={'mlflow.note.content': description})
-    print('Experiment set')
-    return exp_id
-
-
-def mlflow_server(mlruns_folder=False):
-    """
-    Start mlflow server
-    """
-    cmd = ["mlflow", 'server']
-    if mlruns_folder:
-        cmd.extend(['--backend-store-uri', f'file:{mlruns_folder}'])
-    cmd_env = cmd_env = os.environ.copy()
-    child = subprocess.Popen(
-        cmd, env=cmd_env, universal_newlines=True, stdin=subprocess.PIPE
-    )
-    return child
-
-
-class MLRun(MlflowClient):
-    def __init__(self, exp_name: str, description: str, run_id: Optional[str] = None):
-        exp_id = setup_mlflow(exp_name, description)
-        super().__init__(mlflow.get_tracking_uri(), mlflow.get_registry_uri())
-        if run_id is None:
-            self.run = self.create_run(experiment_id=exp_id)
-        else:
-            self.run = self.get_run(run_id)
-
-    def log_params(self, params: Mapping):
-        params = mlflow_linearize(params)
-        for k, v in params.items():
-            self.log_param(k, v)
-
-    def log_metrics(self, metrics: Mapping):
-        for k, v in metrics.items():
-            self.log_metric(k, v)
-
-    def log_param(self, key: str, value: Any, run_id: str = None) -> None:
-        if run_id is None:
-            run_id = self.run.info.run_id
-        super().log_param(run_id, key, value)
-
-    def log_metric(self, key: str, value: Any, run_id: str = None,
-                   timestamp: Optional[int] = None,
-                   step: Optional[int] = None) -> None:
-
-        value = value.item() if type(value) == torch.Tensor else value
-
-        if run_id is None:
-            run_id = self.run.info.run_id
-        super().log_metric(run_id, key, value)
+#
+# def setup_mlflow(exp_name: str, description: str) -> str:
+#     """
+#     Setup mlflow tracking server and experiment
+#     """
+#     tracking_uri = registry_uri = "http://localhost:5000"
+#     mlflow.set_tracking_uri(tracking_uri)
+#     mlflow.set_registry_uri(registry_uri)
+#     print('URI set!')
+#     client = MlflowClient(tracking_uri, registry_uri)
+#     exp_info = client.get_experiment_by_name(exp_name)
+#     exp_id = exp_info.experiment_id if exp_info else \
+#         client.create_experiment(exp_name, tags={'mlflow.note.content': description})
+#     print('Experiment set')
+#     return exp_id
+#
+#
+# def mlflow_server(mlruns_folder=False):
+#     """
+#     Start mlflow server
+#     """
+#     cmd = ["mlflow", 'server']
+#     if mlruns_folder:
+#         cmd.extend(['--backend-store-uri', f'file:{mlruns_folder}'])
+#     cmd_env = cmd_env = os.environ.copy()
+#     child = subprocess.Popen(
+#         cmd, env=cmd_env, universal_newlines=True, stdin=subprocess.PIPE
+#     )
+#     return child
+#
+#
+# class MLRun(MlflowClient):
+#     def __init__(self, exp_name: str, description: str, run_id: Optional[str] = None):
+#         exp_id = setup_mlflow(exp_name, description)
+#         super().__init__(mlflow.get_tracking_uri(), mlflow.get_registry_uri())
+#         if run_id is None:
+#             self.run = self.create_run(experiment_id=exp_id)
+#         else:
+#             self.run = self.get_run(run_id)
+#
+#     def log_params(self, params: Mapping):
+#         params = mlflow_linearize(params)
+#         for k, v in params.items():
+#             self.log_param(k, v)
+#
+#     def log_metrics(self, metrics: Mapping):
+#         for k, v in metrics.items():
+#             self.log_metric(k, v)
+#
+#     def log_param(self, key: str, value: Any, run_id: str = None) -> None:
+#         if run_id is None:
+#             run_id = self.run.info.run_id
+#         super().log_param(run_id, key, value)
+#
+#     def log_metric(self, key: str, value: Any, run_id: str = None,
+#                    timestamp: Optional[int] = None,
+#                    step: Optional[int] = None) -> None:
+#
+#         value = value.item() if type(value) == torch.Tensor else value
+#
+#         if run_id is None:
+#             run_id = self.run.info.run_id
+#         super().log_metric(run_id, key, value)
 
 
 def mlflow_linearize(dictionary: Mapping) -> Mapping:
